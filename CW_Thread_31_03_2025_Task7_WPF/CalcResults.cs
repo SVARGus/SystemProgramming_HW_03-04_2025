df7de2e952ec144ac7cc576d660ea1e368f7c49b
SVARGus <kyznetsovpn@mail.ru>
2025-04-03 17:59:52 +0300

    Задание 7 в процессе реализации

---
+++ CW_Thread_31_03_2025_Task7_WPF/CalcResults.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_Thread_31_03_2025_Task7_WPF
+{
+    public class CalcResults
+    {
+        public int[] Numbers { get; set; }
+        public int Max { get; set; }
+        public int Min { get; set; }
+        public double Average { get; set; }
+    }
+}

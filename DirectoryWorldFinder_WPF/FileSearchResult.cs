fbe3bd6ab927ca042e02a3101faca02f722ff927
SVARGus <kyznetsovpn@mail.ru>
2025-04-10 17:48:28 +0300

    Начало реализации задания №5 (реализован класс аккумулирующий данные после поиска)

---
+++ DirectoryWorldFinder_WPF/FileSearchResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryWorldFinder_WPF
+{
+    public class FileSearchResult
+    {
+        public string fileName { get; set; }
+        public string filePath { get; set; }
+        public int wordCount { get; set; }
+
+        public FileSearchResult(string fileName, string filePath, int wordCount)
+        {
+            this.fileName = fileName;
+            this.filePath = filePath;
+            this.wordCount = wordCount;
+        }
+        public FileSearchResult() { }
+    }
+}
---
+++ DirectoryWorldFinder_WPF/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+/*
+Задание 5
+	Создайте приложения для поиска слова в файлах некоторой директории. 
+    Вход в поддиректории обязателен. 
+    Слово и путь к директории указываются пользователем. 
+    Для решения задачи используйте оконный интерфейс и асинхронность. 
+    По итогам поиска приложение должно показать отчет в формате:
+	Название файла: ……
+	Путь к файлу: ….
+	Количество вхождений слова: ….
+
+*/
+
+namespace DirectoryWorldFinder_WPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

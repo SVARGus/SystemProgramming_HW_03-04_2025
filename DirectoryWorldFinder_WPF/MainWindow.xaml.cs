c2071a82505aa80c0af9297032b0a3d990620f56
SVARGus <kyznetsovpn@mail.ru>
2025-04-11 17:07:49 +0300

    Задание №5 завершено и протестировано

--- DirectoryWorldFinder_WPF/MainWindow.xaml.cs
+++ DirectoryWorldFinder_WPF/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+//using System.Windows.Shapes;
+//using System.Windows.Forms;
+
 
 /*
 Задание 5
@@ -30,9 +23,124 @@ namespace DirectoryWorldFinder_WPF
 {
     public partial class MainWindow : Window
     {
+        private delegate int SearchTextDelegate(string strSearch, string fileName, bool caseSensitive);
+        private List<FileSearchResult> results = new List<FileSearchResult>();
+        private bool cancelRequested = false;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SelectFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            using(var dialog  = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                dialog.Description = "Выберите папку для поиска";
+                dialog.ShowNewFolderButton = false;
+
+                System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+
+                if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(dialog.SelectedPath))
+                {
+                    SelectDerictoriiTextBox.Text = dialog.SelectedPath;
+                }
+            }
+
+        }
+
+        private void StartSearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string searchText = SearchTextBox.Text;
+            string directory = SelectDerictoriiTextBox.Text;
+            bool caseSensitive = CaseSensitiveCheckBox.IsChecked == true;
+
+            if (string.IsNullOrEmpty(searchText) || string.IsNullOrEmpty(directory))
+            {
+                MessageBox.Show("укажите слово для поиска и выберите папку");
+                return;
+            }
+
+            cancelRequested = false;
+            StartSearchButton.IsEnabled = false;
+            ResultsListBox.ItemsSource = null;
+            results.Clear();
+
+            string[] files = Directory.GetFiles(directory, "*", SearchOption.AllDirectories);
+
+            foreach(var file in files)
+            {
+                SearchTextDelegate searchTextDelegate = new SearchTextDelegate(SearchText);
+                searchTextDelegate.BeginInvoke(searchText, file, caseSensitive, SearchCompleted, new object[] { searchTextDelegate, file });
+            }
+
+        }
+
+        private int SearchText(string strSearch, string fileName, bool caseSensitive)
+        {
+            if(cancelRequested)
+            {
+                return 0;
+            }
+
+            int countSearch = 0;
+            string filleContent;
+
+            try
+            {
+                filleContent = File.ReadAllText(fileName);
+            }
+            catch
+            {
+                return 0;
+            }
+
+            if (!caseSensitive)
+            {
+                filleContent = filleContent.ToLower();
+                strSearch = strSearch.ToLower();
+            }
+
+            int index = 0;
+            while ((index = filleContent.IndexOf(strSearch, index)) != -1)
+            {
+                ++countSearch;
+                index += strSearch.Length;
+            }
+
+            return countSearch;
+        }
+
+        private void SearchCompleted(IAsyncResult ar)
+        {
+            object[] state = (object[])ar.AsyncState;
+            SearchTextDelegate del = (SearchTextDelegate)state[0];
+            string file = (string)state[1];
+            int count = del.EndInvoke(ar);
+
+            if(cancelRequested || count == 0)
+            {
+                return;
+            }
+
+            Dispatcher.Invoke(() =>
+            {
+                results.Add(new FileSearchResult
+                {
+                    fileName = Path.GetFileName(file),
+                    filePath = file,
+                    wordCount = count
+                });
+
+                ResultsListBox.ItemsSource = null;
+                ResultsListBox.ItemsSource = results;
+            });
+        }
+
+        private void StoptSearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            cancelRequested = true;
+            StartSearchButton.IsEnabled = true;
         }
     }
 }
